d25bedfe1d83004f912fb1c552f1f7b1b3464792
Steffen Ryll <steffen.ryll@me.com>
2018-12-18 16:58:18 +0100

    Add Bootstrap for Vue.js

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import Vue from "vue";
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-let v = new Vue({
+Vue.use(BootstrapVue);
+
+let app = new Vue({
     el: "#app",
     template: `
     <div>
         <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
+        Name: <b-form-input v-model="name" type="text" />
     </div>`,
     data: {
         name: "World"
