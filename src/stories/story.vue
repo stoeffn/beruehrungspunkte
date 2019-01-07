38d6a77d013c2acbbbc234516caa76377d3e6bfe
la.gand <la.gandyra@gmx.de>
2019-01-07 16:12:47 +0100

    Article

--- src/stories/story.vue
+++ src/stories/story.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Story</h1>
+  <div class="story-container">
+    <div class="story">
+      <img class="story-picture" src="img/story-thumbnail.jpg" />
+      <h1> Trees </h1>
+      <div id="floated"> Some really interesting things about this good ass locking tree. And blablabla blablab lablab labla blablab lablablab lablablabl ablablab lablab labla bl ablabla blablabl ablab lablablab lablabla blablablabl abl ablablablab lablablabl abla blablabl ablablabl ablabl ablabla blabl abla blabla blabla blabl ablab labla bla bla blaalalalalal vlallablalblalblabl alblallablalbla blablalbal blalblalbla blalbalblab ablalbalbl abllablalba balblabl abllablalblabl</div>
+    </div>
+    <div class="story-text" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,4 +16,25 @@ export default Vue.extend({});
 </script>
 
 <style>
+.story-container {
+  display: block;
+  width: 100%;
+}
+
+.story {
+  background-color: white;
+  display: block;
+}
+
+.story-picture {
+  float: left;
+  max-width: 30%;
+  height: auto;
+  margin: 0 20px 20px 0;
+}
+
+#floated {
+  text-align: justify;
+  width: 100%;
+}
 </style>
