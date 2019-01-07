6ea21a979d98285667cfc01c222bbed156c9bb32
la.gand <la.gandyra@gmx.de>
2019-01-07 16:37:52 +0100

    Add Comment section

--- src/stories/story.vue
+++ src/stories/story.vue
@@ -3,9 +3,17 @@
     <div class="story">
       <img class="story-picture" src="img/story-thumbnail.jpg" />
       <h1> Trees </h1>
-      <div id="floated"> Some really interesting things about this good ass locking tree. And blablabla blablab lablab labla blablab lablablab lablablabl ablablab lablab labla bl ablabla blablabl ablab lablablab lablabla blablablabl abl ablablablab lablablabl abla blablabl ablablabl ablabl ablabla blabl abla blabla blabla blabl ablab labla bla bla blaalalalalal vlallablalblalblabl alblallablalbla blablalbal blalblalbla blalbalblab ablalbalbl abllablalba balblabl abllablalblabl</div>
+      <div id="floated"> Some really interesting things about this good ass locking tree. And blablabla blablab lablab labla blablab lablablab lablablabl ablablab lablab labla bl ablabla blablabl ablab lablablab lablabla blablablabl abl ablablablab lablablabl abla blablabl ablablabl ablabl ablabla blabl abla blabla blabla blabl ablab labla bla bla blaalalalalal vlallablalblalblabl alblallablalbla blablalbal blalblalbla blalbalblab ablalbalbl abllablalba balblabl abllablalblabl </div>
     </div>
-    <div class="story-text" />
+    <div class="comments">
+      <h2> Comments </h2>
+      <p> Write your own stupid comment: </p>
+      <form action="writeComment">
+        <input type="text" name="comment">
+      </form>
+      <h3> Top Comments: </h3>
+      <p> stupid </p>
+    </div>
   </div>
 </template>
 
@@ -37,4 +45,19 @@ export default Vue.extend({});
   text-align: justify;
   width: 100%;
 }
+
+.comments {
+  margin-top: 50px;
+}
+
+input {
+  width: 75%;
+  height: 30px;
+}
+
+h3 {
+  margin-top: 40px;
+  font-size: 14px;
+}
+
 </style>
